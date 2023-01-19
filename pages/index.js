74ebb57346191a53fa96d81770dd20f7cbb93d35
Evgeny Shenets <evgenyshenets91@gmail.com>
2023-01-19 17:42:20 +0400

    add initial Head element

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import { EventList } from '@/components/events/EventList';
 import { getAllEvents } from '@/helpers/api-util';
+import Head from 'next/head';
 
 export default function Home(props) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name={'description'}
+          content={'Find a lot of great events that allow to evolve...'}
+        />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
