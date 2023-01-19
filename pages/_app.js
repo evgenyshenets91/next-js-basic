72973cb3d885afb2a2a953c94a77a727a58ea93a
Evgeny Shenets <evgenyshenets91@gmail.com>
2023-01-19 18:13:50 +0400

    add default Head in the _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Fragment } from 'react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>NextJS default page</title>
+        <meta
+          name={'description'}
+          content={'Find a lot of great events that allow to evolve...'}
+        />
+        <meta
+          name={'viewport'}
+          content={'initial-scale=1.0, width=device-width'}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
