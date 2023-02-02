884b035151e50003f782627d65c487ae02166b88
Evgeny Shenets <evgenyshenets91@gmail.com>
2023-02-02 15:38:04 +0400

    store email in mongo db

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+const { MongoClient } = require('mongodb');
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email;
 
@@ -6,6 +8,16 @@ export default function handler(req, res) {
       req.status(422).json({ message: 'Invalid Address.' });
       return;
     }
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://dbUser:EDQV5MHGEQUjbRZW@cluster0.r5s0bwx.mongodb.net/?retryWrites=true&w=majority',
+    );
+
+    const db = client.db();
+
+    await db.collection('email').insertOne({ email: email });
+
+    await client.close();
 
     res.status(201).json({ message: 'Signed Up!' });
   }
