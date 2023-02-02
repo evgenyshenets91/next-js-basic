9da220729534ca607b21583676f6dcc8dab4e6ab
Evgeny Shenets <evgenyshenets91@gmail.com>
2023-02-02 16:52:10 +0400

    add try catch for newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,17 @@
 const { MongoClient } = require('mongodb');
 
+async function connectDataBase() {
+  return await MongoClient.connect(
+    'mongodb+srv://dbUser:EDQV5MHGEQUjbRZW@cluster0.r5s0bwx.mongodb.net/events?retryWrites=true&w=majority',
+  );
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection('newsletter').insertOne(document);
+}
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email;
@@ -9,15 +21,21 @@ export default async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://dbUser:EDQV5MHGEQUjbRZW@cluster0.r5s0bwx.mongodb.net/events?retryWrites=true&w=majority',
-    );
+    try {
+      const client = await connectDataBase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connection to DB failed' });
+      return;
+    }
 
-    const db = client.db();
-
-    await db.collection('newsletter').insertOne({ email: email });
-
-    await client.close();
+    try {
+      const client = await connectDataBase();
+      await insertDocument(client, { email });
+      await client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed' });
+      return;
+    }
 
     res.status(201).json({ message: 'Signed Up!' });
   }
